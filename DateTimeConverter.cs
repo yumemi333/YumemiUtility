f098eb0ef1937602cd1f2741eb0541c914ffa9c4
mitsuda_yumemi <>
2023-07-09 15:42:12 +0900

    Addeded datetime  utility which can  prevent errors is caused by culture difference when use datetime.

---
+++ DateTimeConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+using UnityEngine;
+
+
+/// <summary>
+/// Set specific format to prevent errors which are caused when
+/// convert datetime to string or string to datetime error.
+/// </summary>
+public class DateTimeConverter
+{
+    public static readonly CultureInfo StandardCultureInfo = new CultureInfo("ja-JP");
+
+    public static DateTime StringToDateTime(string str)
+    {
+        DateTime result;
+
+        DateTime.TryParse(str, StandardCultureInfo, DateTimeStyles.None, out result);
+        
+        return result;
+    }
+
+    public static string DateTimeToString(DateTime dateTime, string format = "yyyy/MM/dd HH:mm:ss")
+    {
+        CultureInfo.CurrentCulture = StandardCultureInfo;
+        return dateTime.ToString(format);
+    }
+
+    /// <summary>
+    /// Change the current UI culture to setted culture style
+    /// </summary>
+    /// <returns></returns>
+    public static DateTime DateTimeNow() {
+        CultureInfo.CurrentCulture = StandardCultureInfo;
+        return DateTime.Now;
+    }
+}
+
